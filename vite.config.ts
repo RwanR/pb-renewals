93bbdb429338d335206fa1df5d569475065a492d
Erwann Bocher <erwann.bocher@gmail.com>
2026-05-16 11:01:38 +0200

    fix(pbis): add rollupOptions.external for .prisma/pbis-client

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,9 @@ export default defineConfig({
   ],
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: {
+      external: [".prisma/pbis-client"],
+    },
   },
   optimizeDeps: {
     include: ["@shopify/app-bridge-react"],
