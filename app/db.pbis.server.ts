eef361a12c6e6cd2f799517e4f33e200bc9d9114
Erwann Bocher <erwann.bocher@gmail.com>
2026-05-18 06:39:55 +0200

    chore(pbis): baseline migrations, dedicated PBIS_DATABASE_URL, fix prisma singleton

--- app/db.pbis.server.ts
+++ app/db.pbis.server.ts
@@ -2,15 +2,13 @@ import { PrismaClient } from "@prisma-pbis/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var pbisPrismaGlobal: PrismaClient;
-}
-
-if (process.env.NODE_ENV !== "production") {
-  if (!global.pbisPrismaGlobal) {
-    global.pbisPrismaGlobal = new PrismaClient();
-  }
+  var pbisPrismaGlobal: PrismaClient | undefined;
 }
 
 const pbisDb = global.pbisPrismaGlobal ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.pbisPrismaGlobal = pbisDb;
+}
 
 export default pbisDb;
